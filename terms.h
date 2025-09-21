a1743f37f1e2eb7ed47281785008c4746fdf4ea2
William Pursell <william.r.pursell@gmail.com>
2025-09-21 17:12:22 -0600

    Remove internal tabs
    type: ws

--- terms.h
+++ terms.h
@@ -2,13 +2,13 @@
  * header file for term.c
  */
 
-extern char	*tgoto();
-extern char	PC, *BC, *UP;
+extern char *tgoto();
+extern char PC, *BC, *UP;
 
-extern char	*term_name;
-extern char	XBC[], XUP[];
-extern char	bell_str[];
-extern char     *cursor_home;
+extern char    *term_name;
+extern char     XBC[], XUP[];
+extern char     bell_str[];
+extern char    *cursor_home;
 extern char     clear_screen[];
 extern char     cursor_address[];
 extern char     enter_standout_mode[], exit_standout_mode[];
@@ -19,8 +19,8 @@ extern char     enter_underline_mode[], exit_underline_mode[];
 extern bell();
 extern quiet();
 
-extern int Lines, Columns;	/* screen size */
-extern int garbage_size;	/* number of garbage chars left from so */
-extern int double_garbage;	/* space needed to enter&exit standout mode */
-extern int STANDOUT;		/* terminal got standout mode */
-extern int TWRAP;		/* terminal got automatic margins */
+extern int Lines, Columns; /* screen size */
+extern int garbage_size;   /* number of garbage chars left from so */
+extern int double_garbage; /* space needed to enter&exit standout mode */
+extern int STANDOUT;       /* terminal got standout mode */
+extern int TWRAP;          /* terminal got automatic margins */
