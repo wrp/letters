560a807b3909dc4d8ac2c21ae74ba6d5896d6c21
William Pursell <william.r.pursell@gmail.com>
2025-09-22 05:46:36 -0600

    Remove TURBOC branch in term.h
    type: bld

--- term.h
+++ term.h
@@ -5,23 +5,10 @@
  * Larry Moss (lm03_cif@uhura.cc.rochester.edu)
  */
 
-#ifdef __TURBOC__
-# include <conio.h>
-
-# define putp(str)      puts(str)
-# define HAS_CAP(str)	(*str)
-# define clrdisp()	clrscr()
-# define home()		putp(cursor_home)
-# define goto_xy(c, l)	gotoxy((c)+1, (l)+1)
-
-#else
-
 # define putp(str)      tputs(str, 0, putchar)
 # define HAS_CAP(str)	(*str)
 # define clrdisp()      tputs(clear_screen, Lines, putchar)
 # define home()		putp(cursor_home)
 # define goto_xy(c, l)	putp(tgoto(cursor_address, c, l))
-
-#endif
 
 #define SP ' '
